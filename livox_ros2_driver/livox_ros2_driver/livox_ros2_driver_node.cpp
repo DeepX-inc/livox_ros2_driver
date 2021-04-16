060230de71d224a5f6d7d276db9d11497101f0bd
Nandish Patel <nandish221993@yahoo.in>
2021-04-16 02:12:14 +0000

    [update] enable node options for param from yaml file

--- livox_ros2_driver/livox_ros2_driver/livox_ros2_driver_node.cpp
+++ livox_ros2_driver/livox_ros2_driver/livox_ros2_driver_node.cpp
@@ -7,6 +7,8 @@ int main(int argc, char** argv) {
 
     auto node_name = std::string("livox_driver_node");
     rclcpp::NodeOptions options = rclcpp::NodeOptions();
+    options.allow_undeclared_parameters(true);
+    options.automatically_declare_parameters_from_overrides(true);
 
     auto livox_driver = std::make_shared<livox_ros::LivoxDriver>(
         node_name, options);
